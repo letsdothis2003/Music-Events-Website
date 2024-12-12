53f9aa5d5ad873261505ef42bf3b6be4b9de8d3e
scamp531 <scampeche0531@gmail.com>
2024-12-12 00:27:35 -0500

    Update home.js
    Added blurry image (royalty free)

--- src/home.js
+++ src/home.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './App.css';
+import Blurry from './components/blurryloader'
+
+
 
 function Home() {
   return (
@@ -9,15 +12,11 @@ function Home() {
         <p>
          Home of local NYC music-related events! You can find musical events and memorabillia in areas near you!
         </p>
-        <a
-          className="Home-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div>
+        <Blurry imageUrl="https://images.unsplash.com/photo-1459749411175-04bf5292ceea?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
+        <h1>Join us for one of our concerts!</h1>
+      </div>
     </div>
   );
 }
